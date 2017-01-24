386502969e35136eafab14a402f1f91fd48266d9
timdiels <timdiels.m@gmail.com>
2017-01-24 14:24:25 +0100

    Internal: Fix: after_success.sh
    - #!/usr/bin/env instead of /bin/env
    - missing then on if statement

--- travis_files/after_success.sh
+++ travis_files/after_success.sh
@@ -1,4 +1,4 @@
-#!/bin/env sh
+#!/usr/bin/env sh
 
 # On master branch?
 if [ `git rev-parse --abbrev-ref HEAD` = "master" ]; then
@@ -6,7 +6,7 @@ if [ `git rev-parse --abbrev-ref HEAD` = "master" ]; then
     if [ git name-rev --tags --no-undefined HEAD >/dev/null 2>&1 ]; then
         # Upload to Python index
 
-        if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+        if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
             index=https://testpypi.python.org/pypi
         else
             index=https://pypi.python.org/pypi
