ddf721c3dbbd883bf00c58a032fb26f9458c3204
timdiels <timdiels.m@gmail.com>
2017-01-24 13:14:45 +0100

    Internal: travis: release exactly once, only if all jobs succeeded
    We are using this workaround: https://github.com/alrra/travis-after-all
    
    Do not confuse it with this similarly named one which does not work:
    https://github.com/dmakhno/travis_after_all
    
    Also, use TRAVIS_BRANCH, TRAVIS_TAG. This should fix not getting
    triggered on a PR (where TRAVIS_BRANCH!=git's branch)

--- travis_files/after_success.sh
+++ travis_files/after_success.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env sh
 
-# On master branch?
-if [ `git rev-parse --abbrev-ref HEAD` = "master" ]; then
+# Only run if all jobs succeeded and we are designated to run the after_success
+# (i.e. we ensure it's run only once)
+#
+# This is a workaround, replace it once this has been released: https://github.com/travis-ci/travis-ci/issues/929
+# Workaround: https://github.com/alrra/travis-after-all
+npm install travis-after-all
+travis-after-all
+exit_code=$?
+if [ $exit_code = 2 ]; then
+    # A different runner will do the after_success
+    exit 0
+elif [ $exit_code != 0 ]; then
+    # A job failed or travis-after-all had an internal error
+    exit 1
+fi
+
+# Targetting master branch?
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    echo master
     # Commit is (version) tagged?
-    if [ git name-rev --tags --no-undefined HEAD >/dev/null 2>&1 ]; then
+    if [ ! -z "$TRAVIS_TAG" ]; then
+        echo tagged
         # Upload to Python index
 
         if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
@@ -11,6 +29,8 @@ if [ `git rev-parse --abbrev-ref HEAD` = "master" ]; then
         else
             index=https://pypi.python.org/pypi
         fi
+
+        echo $index
 
         pyb upload_twine --exclude run_unit_tests --exclude run_integration_tests --exclude verify -P "distutils_upload_repositor=$index"
     fi
